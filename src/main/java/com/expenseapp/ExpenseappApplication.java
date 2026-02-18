0c136b2b5cb8710e0e1f460c8bb47391f6bd00ff
__white_walker__ <aj.kolhe9657@gmail.com>
2026-02-18 14:43:45 +0530

    setup base security configuration

--- src/main/java/com/expenseapp/ExpenseappApplication.java
+++ src/main/java/com/expenseapp/ExpenseappApplication.java
@@ -1,4 +1,4 @@
-package com.expenseapp.expenseapp;
+package com.expenseapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
